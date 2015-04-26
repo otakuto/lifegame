d8dbb8215a33f30b60b27e74e9f91e04a5c4fd72
otakuto <otakuto5648@gmail.com>
2015-04-27 00:13:22 +0900

    teki

--- main.hs
+++ main.hs
@@ -1,14 +1,15 @@
+import Foreign.Marshal.Utils
+
 size = 20
 cell = replicate size $ replicate size True
 
-draw [] = return ()
-draw (x:xs) =
-	do
-	print [if y then 'O' else ' ' | y <- x]
-	draw xs
+draw = mapM_ (\xs -> putStrLn [if x then 'O' else ' ' | x <- xs])
 
 main =
 	do
 	draw cell
-	let a = [[2,3,4],[4,5,6],[1,5,6]] in print $ a !! 2 !! 2
-
+	let range = [(x, y) | x <- [0..(size - 1)], y <- [0..(size - 1)]]
+	let search = [(x, y) | x <- [-1..1], y <- [-1..1]]
+	print range
+	print search
+	print . sum $ map (\(x, y) -> fromBool $ cell !! y !! x) range
